9ba1aec829807bb82dd1435e489025538e3297b9
Seyyed Ali Ayati <aliayati752@outlook.com>
2021-05-03 10:23:55 +0430

    Added Download from Internet for out image view

--- app/src/main/java/ir/ac/iust/mnc/carino/CarListAdapter.kt
+++ app/src/main/java/ir/ac/iust/mnc/carino/CarListAdapter.kt
@@ -1,10 +1,16 @@
 package ir.ac.iust.mnc.carino
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import ir.ac.iust.mnc.carino.data.Car
 
@@ -13,11 +19,34 @@ class CarListAdapter(private val carList: List<Car>): RecyclerView.Adapter<CarLi
     class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         val carTitle: TextView
         val carYear: TextView
+        val carImage: ImageView
 
         init {
             Log.i("TEST", "TEST: " + view)
             carTitle = view.findViewById(R.id.txt_title)
             carYear = view.findViewById(R.id.txt_year)
+            carImage = view.findViewById(R.id.img_car)
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    @Suppress("DEPRECATION")
+    private inner class DownloadImageFromInternet(var imageView: ImageView) : AsyncTask<String, Void, Bitmap?>() {
+        override fun doInBackground(vararg urls: String): Bitmap? {
+            val imageURL = urls[0]
+            var image: Bitmap? = null
+            try {
+                val `in` = java.net.URL(imageURL).openStream()
+                image = BitmapFactory.decodeStream(`in`)
+            }
+            catch (e: Exception) {
+                Log.e("Error Message", e.message.toString())
+                e.printStackTrace()
+            }
+            return image
+        }
+        override fun onPostExecute(result: Bitmap?) {
+            imageView.setImageBitmap(result)
         }
     }
 
@@ -33,6 +62,7 @@ class CarListAdapter(private val carList: List<Car>): RecyclerView.Adapter<CarLi
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.carTitle.text = carList[position].name
         holder.carYear.text = carList[position].year.toString()
+        DownloadImageFromInternet(holder.carImage).execute(carList[position].image_path)
     }
 
 }
